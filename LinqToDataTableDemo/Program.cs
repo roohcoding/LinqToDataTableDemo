97933cb20db74bcb23a65de454613c1470507178
Rupali Natewad <rupalirnatewad@gmail.com>
2022-06-06 15:19:38 +0530

    Add project files.

---
+++ LinqToDataTableDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinqToDataTableDemo;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Data Table demo");
+LinqToDataTable linqToDataTable = new LinqToDataTable();
+linqToDataTable.AddToDataTableDemo();
+Console.ReadKey();
